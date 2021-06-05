0bed7f6be8e4540066ba8d00e8b6f3df9f310fea
xeyong <recruyong@gmail.com>
2021-06-06 00:37:15 +0900

    type: delete api delete api seyong

---
+++ src/main/java/com/example/deleteapi/DeleteapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.deleteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeleteapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deleteapi/DeleteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deleteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
